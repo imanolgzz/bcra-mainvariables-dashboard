b3e2559243e1fc9e8c9be1d31a4a9e31ef046bc8
Imanol González <117108948+imanolgzz@users.noreply.github.com>
2024-05-06 15:12:46 -0600

    small changes to test

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -2,7 +2,7 @@ import type { Locale } from "./i18n-config";
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
-const dictionaries = {
+const dictionaries:any = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   es: () => import("./dictionaries/es.json").then((module) => module.default),
 };
